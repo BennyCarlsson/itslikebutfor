12dc42ec7f5199e5359cb92266c9f99f7382d47c
Benny Carlsson <carlsson.benny93@gmail.com>
2017-03-04 15:07:51 +0100

    submit and change view works

--- itslikebutfor/src/App.js
+++ itslikebutfor/src/App.js
@@ -1,24 +1,55 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
 import {FormControl, ControlLabel} from 'react-bootstrap';
 
 class App extends Component {
+  constructor(props) {
+   super(props);
+   this.state = {
+     showPage: 'inputfield',
+     lastApp: '',
+   };
+  }
+  lastAppEdit(word){
+    this.setState({lastApp:word, showPage:'showResult'});
+  }
   render() {
     return (
       <div className="App">
         <div id="content">
-          <InputField/>
+          {this.state.showPage === "inputfield"
+            ?<InputField showPage={this.state.showPage} lastAppEdit={this.lastAppEdit.bind(this)} lastApp={this.state.lastApp}/>
+            :<ShowResult showPage={this.state.showPage} lastApp={this.state.lastApp}/>}
         </div>
       </div>
     );
   }
 }
 class ShowResult extends Component{
+  constructor(props) {
+   super(props);
+   this.state = {
+     thing: '',
+   };
+  }
+  componentDidMount(){
+    this.setState({thing:"cats"});
+  }
+  handleClick(e){
+    //backspace
+    if(e.charCode === 8){
+      this.props.lastApp = "";
+    }
+    //enter och space
+    if(e.charCode === 13 || e.charCode === 32){
+
+    }
+  }
   render(){
     return(
-      <div id="resultDiv">
-        <h1>it's like ... but for ...</h1>
+      <div id="resultDiv" onKeyPress={this.handleClick}>
+        <h1>it's like <span className="word">{this.props.lastApp} </span>
+          but for <span className="word">{this.state.thing}</span></h1>
       </div>
     )
   }
@@ -30,32 +61,28 @@ class InputField extends Component{
      value: '',
    };
   }
-  getInitialState() {
-    return {
-      value: ''
-    };
-  }
   handleChange(e) {
+    e.preventDefault();
     this.setState({ value: e.target.value });
   }
   handleSubmit(e){
     e.preventDefault();
+    this.props.lastAppEdit(this.state.value);
   }
   render(){
     return(
       <div className="inputField">
-        <form onSubmit={this.handleSubmit}>
+        <form onSubmit={this.handleSubmit.bind(this)}>
         <ControlLabel>Enter your last used application</ControlLabel>
           <FormControl autoFocus className="formControl"
             type="text"
             value={this.state.value}
             placeholder="e.g. facebook"
-            onChange={this.handleChange}
+            onChange={this.handleChange.bind(this)}
           />
       </form>
       </div>
     )
   }
 }
-
 export default App;
